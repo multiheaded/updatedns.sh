e27ef462dd5802f359c269d9ffdb81f53fbfc7e7
Marc Priggemeyer <marc.priggemeyer@gmail.com>
2023-03-09 18:17:51 +0100

    first version

--- README.md
+++ README.md
@@ -1,2 +1,86 @@
 # updatedns.sh
 Shell script to update DNS entries via RFC 2136
+
+## Installation
+
+`updatedns.sh` depends on some basic programs most of which are probably part of your Linux distribution BSD or Mac operating system. Just make sure these are installed on your system.
+```bash
+ECHO=$PREFIX/bin/echo
+WGET=$PREFIX/bin/wget
+GREP=$PREFIX/bin/grep
+NSUPDATE=$PREFIX/bin/nsupdate
+DIG=$PREFIX/bin/dig
+JQ=$PREFIX/bin/jq
+```
+
+Download or clone this repository and adapt the `config.json` file (see #configuration).
+
+## Configuration
+```json
+{
+    "updatedns" : [
+        {
+            "Keyfile"     : "tsig.key",
+            "IPv4Service" : "http://checkip.dyndns.com/index.html",
+            "IPv6Service" : "http://checkipv6.dyndns.com/index.html",
+            "DNSServer"   : "your.dns.com",
+            "Zone"        : "yourzone.org",
+            "Host"        : "yourhost.yourzone.org",
+            "TTL"         : "900"
+        }
+    ]
+}
+```
+### Keyfile
+A file containing the signature key nsupdate uses to send the RFC 2136 requests. This can be generated using tsig-keygen and should also be present in your DNS configuration to allow updates.
+```json
+key "<name>" {
+        algorithm <algorithm>;
+        secret "<base64 encoded key>";
+};
+```
+
+#### name
+The name of the key.
+#### algorithm
+For example hmac-sha256 or hmac-sha512.
+#### base64 encoded key
+A shared secret
+
+#### Example
+Do NOT use this key in your setup, generate your own!
+```bash
+# tsig-keygen -a hmac-sha512 somezone.com
+key "somezone.com" {
+        algorithm hmac-sha512;
+        secret "DaRVLuRRqU+LWibT2TDsrqhNyPdsGZQUNbTWHC0ktQKuQlMP5qL+jk8fMRJupS2JWZghjPvBYOJGUBbhQtL6qA==";
+};
+```
+
+## Automatic and periodic runs
+There are various options to have your computer run updatedns.sh periodically.
+
+### systemd
+You can utilize the systemd user instance to provide a unit that periodically restarts updatedns.sh.
+```bash
+[Unit]
+Description=Check and update dns entries with updatedns.sh
+
+[Service]
+ExecStart=/home/pi/updatedns/updatedns.sh
+WorkingDirectory=/home/pi/updatedns
+
+RestartSec=180
+Restart=always
+
+[Install]
+WantedBy=default.target
+```
+Here the unit expects the updatedns.sh script to reside in the directory `/home/pi/updatedns`. Adapt the paths in the `ExecStart` and `WorkingDirectory` lines.
+Additionally, adapt the restart policy to your liking (`RestartSec` and `Restart`) to only run once (`Restart=no`) or with a different delay between starts (`RestartSec=300` for five minutes).
+
+### cron
+```
+#minute hour    mday    month   wday    command
+*/3     *       *       *       *       /usr/bin/bash /home/pi/updatedns/updatedns.sh
+```
---
+++ updatedns.sh
@@ -0,0 +1,154 @@
+#!/usr/bin/env bash
+
+SCRIPT=$(readlink -f $0)
+SCRIPTPATH=$(dirname $SCRIPT)
+
+CONFIGFILE="${SCRIPTPATH}/config.json"
+
+PREFIX=/usr
+ECHO=$PREFIX/bin/echo
+WGET=$PREFIX/bin/wget
+GREP=$PREFIX/bin/grep
+NSUPDATE=$PREFIX/bin/nsupdate
+DIG=$PREFIX/bin/dig
+JQ=$PREFIX/bin/jq
+
+UTILITIESFOUND=0
+for prog in $ECHO $WGET $GREP $NSUPDATE $DIG $JQ
+do
+    test -x $prog || { echo "This program depends on the executable ${prog}! Please install it." >&2; UTILITIESFOUND=1; } 
+done
+
+if [ $UTILITIESFOUND -eq 1 ]
+then
+    exit 1
+fi
+
+
+args=`getopt c: $*`
+
+set --  ${args}
+while true; do
+    case "${1}" in 
+        -c)
+            CONFIGFILE="${2}"
+	    shift; shift;
+            ;;
+        --)
+            shift; break;
+            ;;
+    esac
+done
+
+if ! [ -e ${CONFIGFILE} ]
+then
+    ${ECHO} "Config file \"${CONFIGFILE}\" not found! Exiting..." >&2
+    exit 1
+fi
+
+#only support one entry for now
+KEYFILE=$(${JQ} -r '.updatedns[0].Keyfile' ${CONFIGFILE})
+IPV6SITE=$(${JQ} -r '.updatedns[0].IPv6Service' ${CONFIGFILE})
+IPV4SITE=$(${JQ} -r '.updatedns[0].IPv4Service' ${CONFIGFILE})
+DNSSERVER=$(${JQ} -r '.updatedns[0].DNSServer' ${CONFIGFILE})
+ZONE=$(${JQ} -r '.updatedns[0].Zone' ${CONFIGFILE})
+HOST=$(${JQ} -r '.updatedns[0].Host' ${CONFIGFILE})
+TTL=$(${JQ} -r '.updatedns[0].TTL' ${CONFIGFILE})
+
+SETTINGSPRESENT=0
+for C in "KEYFILE" "IPV6SITE" "IPV4SITE" "DNSSERVER" "ZONE" "HOST" "TTL"
+do
+    VALUE="${!C}"
+    echo "${VALUE}"
+    test -z "${VALUE}" || test "${VALUE}" = "null" && { echo "${C} has not been properly configured." >&2; SETTINGSPRESENT=1; } 
+done
+
+if [ $SETTINGSPRESENT -eq 1 ]
+then
+    echo "Exiting..." >&2
+    exit 1
+fi
+
+IPV4REGEX='([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})'
+IPV6REGEX="(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))"
+
+getIPAddr () {
+    local CMD=$1
+    local REGEX=$2
+    local IP=$(${CMD} | ${GREP} -E -o ${REGEX})
+
+    if [ ! -z "$IP" ]
+    then
+        echo $IP
+        return 0
+    else
+        return 1
+    fi
+}
+
+collectIPv4Addr() {
+    getIPAddr "$WGET --quiet -O - $IPV4SITE" "$IPV4REGEX"
+    return $?
+}
+
+collectIPv6Addr() {
+    getIPAddr "$WGET --quiet -O - $IPV6SITE" "$IPV6REGEX"
+    return $?
+}
+
+digAssignedIPv4Addr() {
+    getIPAddr "${DIG} +noall +answer ${HOST} @${DNSSERVER} A" "$IPV4REGEX"
+    return $?
+}
+
+digAssignedIPv6Addr() {
+    getIPAddr "${DIG} +noall +answer ${HOST} @${DNSSERVER} AAAA" "$IPV6REGEX"
+    return $?
+}
+
+generateRecordUpdateStr() {
+    local FINDIP=$1
+    local DIGIP=$2
+    local RECORDTYPE=$3
+
+    local SHOULDUPDATE=1
+    local UPDATESTR=""
+
+    local CURRENTIP=$($FINDIP)
+    if [ $? -eq 0 ]
+    then
+        local DUGIP=$($DIGIP)
+        if [ "a${CURRENTIP}" != "a${DUGIP}" ]
+        then
+            SHOULDUPDATE=0
+            UPDATESTR="${UPDATESTR}$(${ECHO} -n "update delete ${HOST} ${RECORDTYPE}\n")"
+            UPDATESTR="${UPDATESTR}$(${ECHO} -n "update add ${HOST} ${TTL} ${RECORDTYPE} ${CURRENTIP}\n")"
+        else
+            echo "No need to update ${RECORDTYPE} record, is set to ${CURRENTIP}" >&2
+        fi
+    fi
+
+    ${ECHO} -n "$UPDATESTR"
+    return $SHOULDUPDATE
+}
+
+generateUpdateQuery() {
+    local QUERY=$(${ECHO} -n "server ${DNSSERVER}\nzone ${ZONE}\n" )
+
+    QUERY="${QUERY}$(generateRecordUpdateStr collectIPv4Addr digAssignedIPv4Addr A)"
+    QUERY="${QUERY}$(generateRecordUpdateStr collectIPv6Addr digAssignedIPv6Addr AAAA)"
+
+    QUERY="${QUERY}$(${ECHO} -n "show\nsend\n")"
+
+    ${ECHO} "$QUERY" | grep "update add" >/dev/null 2>&1 && { ${ECHO} -n "$QUERY"; return 0; }
+    return 1
+}
+
+QUERY=$(generateUpdateQuery)
+
+if [ $? -eq 0 ]
+then
+    ${ECHO} -e -n $QUERY # | ${NSUPDATE} -k ${KEYFILE} >&2
+fi
+
+
